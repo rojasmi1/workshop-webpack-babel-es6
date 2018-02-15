abef8de94ef969a645fd78a5873cc5f7c3d2f4fc
Michael Rojas <michael.rojasp@gmail.com>
2018-02-15 07:49:04 -0600

    Base project structure with webpack

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import _ from 'lodash';
+import './assets/styles/style.css';
+import laptopIcon from './assets/images/laptop-icon.png';
+import Label from 'Components/label';
+import CustomImage from 'Components/customImage';
+
+function component() {
+const element = document.createElement('div');
+element.classList.add('hello');
+
+// Add label component
+const myLabel = new Label('Sample Label', ['label']);
+element.appendChild(myLabel.render());
+
+// Add the image to our existing div.
+const laptopImageTag = new CustomImage(laptopIcon, ['icon']);
+element.appendChild(laptopImageTag.render());
+
+return element;
+}
+
+document.body.appendChild(component());
